01dcb072030e505c9393a50ee836ef2f27d9be10
Sol Borjas <soltborjas@gmail.com>
2019-04-17 09:57:00 -0400

    initial commit

---
+++ views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+# Add this view for ABOUT HomePageView
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
+
+class ProjectsPageView(TemplateView):
+    template_name = "projects.html"
+
+# Create your views here.
