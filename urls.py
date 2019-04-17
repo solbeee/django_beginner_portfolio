6fbf9968381d63201dba180ae2fb58d782ca387f
Sol Borjas <soltborjas@gmail.com>
2019-04-17 12:40:38 -0400

    added working CSS to all stylesheets

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from helloworld import views
 
-
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()),
