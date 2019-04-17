f3655afa5001cd65cf4ffe3aa5a10c3ae57c72b3
solbeee <45700553+solbeee@users.noreply.github.com>
2019-04-17 12:57:35 -0400

    Add files via upload

---
+++ helloworld/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from helloworld import views
+
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()),
+    url(r'^about/contact/$', views.ContactPageView.as_view()),
+    url(r'^about/contact/projects/$', views.ProjectsPageView.as_view()),
+]
